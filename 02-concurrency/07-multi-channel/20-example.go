113456169c0de5160e1bb5b833e22c99d690c09f
Magesh Kuppan <tkmagesh77@gmail.com>
2024-12-02 15:17:50 +0530

    multiple channel orchestration

---
+++ 02-concurrency/07-multi-channel/20-example.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+func main() {
+	ch1 := make(chan int)
+	ch2 := make(chan int)
+
+	go func() {
+		time.Sleep(5 * time.Second)
+		ch1 <- 100
+	}()
+
+	go func() {
+		time.Sleep(2 * time.Second)
+		ch2 <- 200
+	}()
+
+	/*
+		fmt.Println(<-ch1)
+		fmt.Println(<-ch2)
+	*/
+
+	wg := sync.WaitGroup{}
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		fmt.Println(<-ch1)
+	}()
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		fmt.Println(<-ch2)
+	}()
+
+	wg.Wait()
+}
