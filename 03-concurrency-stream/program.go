74a47796e807fd809cc3ce8ea196b5a19498ca91
Magesh Kuppan <tkmagesh77@gmail.com>
2024-12-03 09:55:05 +0530

    concurrency stream - solution

--- 03-concurrency-stream/program.go
+++ 03-concurrency-stream/program.go
@@ -6,35 +6,93 @@ import (
 	"log"
 	"os"
 	"strconv"
+	"sync"
 )
 
 func main() {
-	total := SumFile("data1.dat")
-	WriteFile("test.txt", fmt.Sprintf("total : %d", total))
+	wg := &sync.WaitGroup{}
+	dataCh := make(chan int)
+	wg.Add(1)
+	go Source("data1.dat", dataCh, wg)
+
+	wg.Add(1)
+	go Source("data2.dat", dataCh, wg)
+
+	evenCh, oddCh := Splitter(dataCh)
+
+	evenSumCh := Sum(evenCh)
+	oddSumCh := Sum(oddCh)
+
+	done := Merger(evenSumCh, oddSumCh)
+
+	wg.Wait()
+	close(dataCh)
+
+	<-done
 }
 
-func SumFile(fileName string) int {
+func Source(fileName string, dataCh chan<- int, wg *sync.WaitGroup) {
+	defer wg.Done()
 	file, err := os.Open(fileName)
 	if err != nil {
 		log.Fatalln(err)
 	}
 	defer file.Close()
 	scanner := bufio.NewScanner(file)
-	total := 0
 	for scanner.Scan() {
 		line := scanner.Text()
 		if no, err := strconv.Atoi(line); err == nil {
-			total += no
+			dataCh <- no
 		}
 	}
-	return total
 }
 
-func WriteFile(fileName string, data string) {
-	file, err := os.Create(fileName)
-	if err != nil {
-		log.Fatalln(err)
-	}
-	defer file.Close()
-	fmt.Fprintln(file, data)
+func Splitter(dataCh chan int) (<-chan int, <-chan int) {
+	evenCh := make(chan int)
+	oddCh := make(chan int)
+	go func() {
+		defer close(evenCh)
+		defer close(oddCh)
+		for no := range dataCh {
+			if no%2 == 0 {
+				evenCh <- no
+			} else {
+				oddCh <- no
+			}
+		}
+	}()
+	return evenCh, oddCh
+}
+
+func Sum(ch <-chan int) <-chan int {
+	sumCh := make(chan int)
+	go func() {
+		var total int
+		for val := range ch {
+			total += val
+		}
+		sumCh <- total
+	}()
+	return sumCh
+}
+
+func Merger(evenSumCh, oddSumCh <-chan int) <-chan struct{} {
+	doneCh := make(chan struct{})
+	go func() {
+		file, err := os.Create("result.txt")
+		if err != nil {
+			log.Fatalln(err)
+		}
+		defer file.Close()
+		for range 2 {
+			select {
+			case evenSum := <-evenSumCh:
+				fmt.Fprintf(file, "Even Total : %d\n", evenSum)
+			case oddSum := <-oddSumCh:
+				fmt.Fprintf(file, "Odd Total : %d\n", oddSum)
+			}
+		}
+		close(doneCh)
+	}()
+	return doneCh
 }
